2e3babe5a77c55dfe84d2c94b13d25970ad7f3b9
Anton <kubana6@gmail.com>
2020-09-29 14:00:10 +0300

    init: make task week 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  let number = 0 
+  if (array.length > 0 ) {
+    number = array.reduce ((acc, rec) => {
+        if(rec < acc) {
+          return rec
+        }
+        return acc
+    }, 0 )
+  }
+  return number
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  let number = 0 
+  if (array.length > 0 ) {
+    number = array.reduce ((acc, rec) => {
+        if(rec > acc) {
+          return rec
+        }
+        return acc
+    }, 0 )
+  }
+  return number
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  let result = 0
+  let number = 0  
+  if (array.length > 0 ) {
+    number = array.reduce ((acc, rec) => {
+        return acc+rec
+    }, 0 )
+    result = number/ (array.length )
+  }
+  return result 
 }
